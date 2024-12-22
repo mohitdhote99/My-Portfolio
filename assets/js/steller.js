5fb5d3b66673a0bde4bcfde4f82a5648764cead9
Mohit Dhote <mohitdhote99@gmail.com>
2024-12-23 03:24:39 +0530

    initial commit

---
+++ assets/js/steller.js
@@ -0,0 +1,68 @@
+/*!
+=========================================================
+* Steller Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+//global variable for collecting form data
+var form = document.getElementById("mail-mohit-form");
+
+
+// smooth scroll
+$(document).ready(function () {
+	$(".nav-link").on('click', function (event) {
+
+		if (this.hash !== "") {
+
+			event.preventDefault();
+
+			var hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 700, function () {
+				window.location.hash = hash;
+			});
+		}
+	});
+});
+
+//call back function to handel email service to my account
+
+let handleSubmit = async(event)=> {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form"
+          }
+        })
+      }
+    }).catch(error => {
+      status.innerHTML = "Oops! There was a problem submitting your form"
+    });
+  }
+
+form.addEventListener("submit", handleSubmit);
